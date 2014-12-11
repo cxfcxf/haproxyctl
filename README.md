81ae47df0db3d5e611c6456af1043fad391c4dd8
cxfcxf <siegfried.chen@gmail.com>
2014-12-11 03:48:54 -0800

    update 12/13/2014

--- README.md
+++ README.md
@@ -1,25 +1,22 @@
-#This is a partial rewrite of https://github.com/flores/haproxyctl in go
+#Haproxyctl in golang
+#####This is a partial rewrite of https://github.com/flores/haproxyctl in go
+thanks for cflores' great work on the original haproxyctl
+i am working towards to complete this when i have time, so we will have compiled binary in the future.
 
-the programing is still under construction. what you see is not completed yet.
 #current status
+although most of original haproxyctl function are usable, 
+the programing is still under actively developing. what you see is not completed yet.
 ```
-1. need to finish flag
-2. more function will be added
-3. seprate the library for exec for library usage
-4. bindding it to a port for remote access and execution
+1. need to finish flag and usage setting
+2. more functions will be added
+3. seprate the library and command execution
+4. binding fucntion, binding it to a port for remote access and execution
 ```
 
 ```
 i am not a expert of haproxy, but i find its actually annonying to gather data or enable/disable servers
 through stat socket haproxy provides. so this is a tool or wrapper to get around with that. 
 ```
-###further
-```
-i am thinking ofbinding the socket to a port with net/http to provide remote accessesbility in the future.
-```
-
-thanks for cflores' great work on the original haproxyctl
-i am working towards to complete this when i have time, so we will have compiled binary in the future.
 
 ###compile haproxyctl.go
 ```
@@ -72,6 +69,19 @@ now printing BACKEND Health Check
 static     BACKEND    DOWN       0
 app        BACKEND    DOWN       0
 ```
+
+disable/enable a server in all backend
+```
+go run haproxyctl.go -disableall="app1"
+
+go run haproxyctl.go -enableall="app1"
+```
+
+exectution socket command (directly execution socket command)
+```
+go run haproxyctl.go -socketexec="get weight app/app1"
+```
+
 
 #License
 This code is released under the MIT License. You should feel free to do whatever you want with it. 
--- haproxyctl.go
+++ haproxyctl.go
@@ -11,12 +11,16 @@ import (
 )
 
 var (
-	showhealth = flag.Bool("showhealth", false, "switch for health check")
-	showbackend = flag.Bool("showbackend", false, "switch for backend info")
-	status     = flag.Bool("status", false, "switch for current running status")
-	enable     = flag.String("enable", "", "enable px/sv / put px/sv into ACTIV mode")
-	disable    = flag.String("disable", "", "disable px/sv / put px/sv into MAINT mode")
-	f	   = flag.String("f", "/etc/haproxy/haproxy.cfg", "point configration file, default /etc/haproxy/haproxy.cfg")
+	configcheck  = flag.Bool("configcheck", false, "check config file")
+	showhealth   = flag.Bool("showhealth", false, "switch for health check")
+	showbackends = flag.Bool("showbackends", false, "switch for backend info")
+	status       = flag.Bool("status", false, "switch for current running status")
+	enable       = flag.String("enable", "", "enable px/sv / put px/sv into ACTIV mode")
+	enableall    = flag.String("enableall", "", "re-enable a server on multiple backends")
+	disable      = flag.String("disable", "", "disable px/sv / put px/sv into MAINT mode")
+	disableall   = flag.String("disableall", "", "disenable a server on multiple backends")
+	socketexec   = flag.String("socketexec", "", "execution socket commands")
+	f            = flag.String("f", "/etc/haproxy/haproxy.cfg", "point configration file, default /etc/haproxy/haproxy.cfg")
 )
 
 const (
@@ -27,9 +31,11 @@ type haProxy struct {
 	Pid  string
 	Sock string
 	Bin  string
+	Cfg  string
 }
 
 func (h *haProxy) Loadenv(cfg string) {
+	h.Cfg = cfg
 	//Load sock and pid to haProxy struct
 	repf := regexp.MustCompile(`pidfile`)
 	reso := regexp.MustCompile(`stats\ socket`)
@@ -38,17 +44,24 @@ func (h *haProxy) Loadenv(cfg string) {
 	cn := strings.Split(string(c), "\n")
 	for _, line := range cn {
 		if repf.MatchString(line) {
-			p, _ :=  ioutil.ReadFile(strings.Fields(line)[1])
+			p, _ := ioutil.ReadFile(strings.Fields(line)[1])
 			h.Pid = strings.Trim(string(p), "\n")
 		}
 		if reso.MatchString(line) {
 			h.Sock = strings.Fields(line)[2]
 		}
 	}
-	//Load binary path to 
+	//Load binary path to haProxy struct
+	rewh := regexp.MustCompile(`no haproxy in`)
 	
-	
-
+	shell := fmt.Sprintf("which haproxy")
+	cmd := exec.Command("sh", "-c", shell)
+	res, _ := cmd.CombinedOutput()
+	if rewh.MatchString(string(res)) {
+		fmt.Println("your haproxy binary is not in the $PATH")	
+	} else {
+		h.Bin = strings.Trim(string(res), "\n")
+	}
 }
 
 func (h *haProxy) Showstatus() {
@@ -116,11 +129,51 @@ func (h *haProxy) DisableServer(px string, sv string) {
 	h.ShowRegexp(sv)
 }
 
+func (h *haProxy) DisableAll(server string) {
+	re := regexp.MustCompile(server)
+
+	output := h.Exec("show stat")
+	out := strings.Split(output, "\n")
+	for i, line := range out {
+		if i != 0 && line != "" {
+			data := strings.Split(line, ",")
+			if re.MatchString(data[1]) && data[17] == "UP" {
+				c := fmt.Sprintf("disable server %s/%s", data[0], server)
+				h.Exec(c)
+			}
+		}
+	}
+}
+
 func (h *haProxy) EnableServer(px string, sv string) {
 	c := fmt.Sprintf("enable server %s/%s", px, sv)
 	h.Exec(c)
 	fmt.Printf("Server %s/%s has been enabled\n", px, sv)
 	h.ShowRegexp(sv)
+}
+
+func (h *haProxy) EnableAll(server string) {
+        re := regexp.MustCompile(server)
+	rest := regexp.MustCompile(`(?i)Down|MAINT`)
+
+        output := h.Exec("show stat")
+        out := strings.Split(output, "\n")
+        for i, line := range out {
+                if i != 0 && line != "" {
+                        data := strings.Split(line, ",")
+                        if re.MatchString(data[1]) && rest.MatchString(data[17]) {
+                                c := fmt.Sprintf("enable server %s/%s", data[0], server)
+                                h.Exec(c)
+                        }
+                }
+        }
+}
+
+func (h *haProxy) Configcheck() {
+	shell := fmt.Sprintf("%s -c -f %s", h.Bin, h.Cfg)
+	cmd := exec.Command("sh", "-c", shell)
+	res, _ := cmd.CombinedOutput()
+	fmt.Println(strings.Trim(string(res), "\n"))
 }
 
 func main() {
@@ -132,11 +185,28 @@ func main() {
 		haproxy.Loadenv("/etc/haproxy/haproxy.cfg")
 	}
 
+	if len(*socketexec) > 0 {
+		res := haproxy.Exec(*socketexec)
+		fmt.Println(res)
+	}
+
+	if *configcheck {
+		haproxy.Configcheck()
+	}
+
+	if *disableall != "" {
+		haproxy.DisableAll(*disableall)
+	}
+	
+	if *enableall != "" {
+		haproxy.EnableAll(*enableall)
+	}
+
 	if *status {
 		haproxy.Showstatus()
 	}
 
-	if *showbackend {
+	if *showbackends {
 		haproxy.ShowRegexp("BACKEND")
 	}
 
@@ -150,6 +220,6 @@ func main() {
 	if *disable != "" {
 		dis := strings.Split(*disable, "/")
 		haproxy.DisableServer(dis[0], dis[1])
-	} 
+	}
 	// flag.PrintDefaults()
 }

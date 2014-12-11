aa1bd7ab382ba198cb56fc7a669d44ee7f889e98
root <root@haproxy.com>
2014-12-11 01:15:44 -0800

    update

--- haproxyctl.go
+++ haproxyctl.go
@@ -1,102 +1,155 @@
 package main
 
 import (
-	"fmt"
-	"net"
 	"flag"
-	"strings"
-	"regexp"
+	"fmt"
 	"io/ioutil"
+	"net"
+	"os/exec"
+	"regexp"
+	"strings"
 )
 
 var (
-	stats = flag.Bool("stats", false, "check status")
-	enable = flag.String("enable", "", "enable px/sv / put px/sv into ACTIV mode")
-	disable = flag.String("disable", "", "disable px/sv / put px/sv into MAINT mode")
+	showhealth = flag.Bool("showhealth", false, "switch for health check")
+	showbackend = flag.Bool("showbackend", false, "switch for backend info")
+	status     = flag.Bool("status", false, "switch for current running status")
+	enable     = flag.String("enable", "", "enable px/sv / put px/sv into ACTIV mode")
+	disable    = flag.String("disable", "", "disable px/sv / put px/sv into MAINT mode")
+	f	   = flag.String("f", "/etc/haproxy/haproxy.cfg", "point configration file, default /etc/haproxy/haproxy.cfg")
 )
 
 const (
-	SOCKET_TYPE	= "unix"
+	SOCKET_TYPE = "unix"
 )
 
 type haProxy struct {
-	Sock	string
+	Pid  string
+	Sock string
+	Bin  string
 }
 
-func (h haProxy) Exec(command string) string {
+func (h *haProxy) Loadenv(cfg string) {
+	//Load sock and pid to haProxy struct
+	repf := regexp.MustCompile(`pidfile`)
+	reso := regexp.MustCompile(`stats\ socket`)
+
+	c, _ := ioutil.ReadFile(cfg)
+	cn := strings.Split(string(c), "\n")
+	for _, line := range cn {
+		if repf.MatchString(line) {
+			p, _ :=  ioutil.ReadFile(strings.Fields(line)[1])
+			h.Pid = strings.Trim(string(p), "\n")
+		}
+		if reso.MatchString(line) {
+			h.Sock = strings.Fields(line)[2]
+		}
+	}
+	//Load binary path to 
+	
+	
+
+}
+
+func (h *haProxy) Showstatus() {
+	if len(h.Pid) > 0 {
+		fmt.Printf("haproxy is running on pid %s.\nthese ports are used and guys are connected:\n", h.Pid)
+		shell := fmt.Sprintf("lsof -ln -i |awk '$2 ~ /%s/ {print $8\" \"$9}'", h.Pid)
+		cmd := exec.Command("sh", "-c", shell)
+		res, _ := cmd.CombinedOutput()
+		fmt.Println(string(res))
+	} else {
+		fmt.Printf("haproxy is not running\n")
+	}
+}
+
+func (h *haProxy) Exec(command string) string {
 	sock, err := net.Dial(SOCKET_TYPE, h.Sock)
-    if err != nil { panic(err) }
-    defer sock.Close()
+	if err != nil {
+		panic(err)
+	}
+	defer sock.Close()
 
-    cmd := fmt.Sprintf("%s\r\n", command)
-    _, err = sock.Write([]byte(cmd))
-    if err != nil { panic(err) }
+	cmd := fmt.Sprintf("%s\r\n", command)
+	_, err = sock.Write([]byte(cmd))
+	if err != nil {
+		panic(err)
+	}
 
-    res, err := ioutil.ReadAll(sock)
-    if err != nil { panic(err) }
-    return string(res)
+	res, err := ioutil.ReadAll(sock)
+	if err != nil {
+		panic(err)
+	}
+	return string(res)
 }
 
-func (h haProxy) Showhealth() {
-    output := h.Exec("show stat")
-    out := strings.Split(output, "\n")
-	fmt.Println("now printing Health Check")
-	fmt.Printf("=====================================\n")
-    for _, line := range out {
-        if line != "" {
-            data := strings.Split(line, ",")
-            fmt.Printf("%-10s %-10s %-10s %-10s\n", data[0], data[1], data[17], data[18])
-        }
-    }
-	fmt.Printf("=====================================\n\n\n")
+func (h *haProxy) Showhealth() {
+	output := h.Exec("show stat")
+	out := strings.Split(output, "\n")
+	fmt.Printf("\nnow printing Health Check...\n\n")
+	for _, line := range out {
+		if line != "" {
+			data := strings.Split(line, ",")
+			fmt.Printf("%-10s %-10s %-10s %-10s\n", data[0], data[1], data[17], data[18])
+		}
+	}
 }
 
-func (h haProxy) ShowRegexp(reg string) {
+func (h *haProxy) ShowRegexp(reg string) {
 	re := regexp.MustCompile(reg)
 
-    output := h.Exec("show stat")
-    out := strings.Split(output, "\n")
-	fmt.Printf("now printing %s Health Check\n", reg)
-	fmt.Printf("=====================================\n")
-    for i, line := range out {
-        if i==0 || re.MatchString(line) {
-            data := strings.Split(line, ",")
-            fmt.Printf("%-10s %-10s %-10s %-10s\n", data[0], data[1], data[17], data[18])
-        }
-    }
-	fmt.Printf("=====================================\n\n\n")
-
+	output := h.Exec("show stat")
+	out := strings.Split(output, "\n")
+	fmt.Printf("\nnow printing %s Health Check\n", reg)
+	for i, line := range out {
+		if i == 0 || re.MatchString(line) {
+			data := strings.Split(line, ",")
+			fmt.Printf("%-10s %-10s %-10s %-10s\n", data[0], data[1], data[17], data[18])
+		}
+	}
 }
 
-func (h haProxy) DisableServer(px string, sv string) {
+func (h *haProxy) DisableServer(px string, sv string) {
 	c := fmt.Sprintf("disable server %s/%s", px, sv)
 	h.Exec(c)
 	fmt.Printf("Server %s/%s has been disabled\n", px, sv)
 	h.ShowRegexp(sv)
 }
 
-func (h haProxy) EnableServer(px string, sv string) {
-    c := fmt.Sprintf("enable server %s/%s", px, sv)
-    h.Exec(c)
+func (h *haProxy) EnableServer(px string, sv string) {
+	c := fmt.Sprintf("enable server %s/%s", px, sv)
+	h.Exec(c)
 	fmt.Printf("Server %s/%s has been enabled\n", px, sv)
-    h.ShowRegexp(sv)
+	h.ShowRegexp(sv)
 }
 
 func main() {
 	flag.Parse()
-   	haproxy := new(haProxy)
-   	haproxy.Sock = "/var/lib/haproxy/stats"
-	
-	if *stats {
-		haproxy.Showhealth()
-	} else if *enable != "" {
-		en := strings.Split(*enable, "/")
-		haproxy.EnableServer(en[0], en[1])
-	} else if *disable != "" {
-		dis := strings.Split(*disable, "/")
-		haproxy.DisableServer(dis[0], dis[1])
+	haproxy := new(haProxy)
+	if len(*f) > 0 {
+		haproxy.Loadenv(*f)
 	} else {
-		flag.PrintDefaults()
+		haproxy.Loadenv("/etc/haproxy/haproxy.cfg")
 	}
 
+	if *status {
+		haproxy.Showstatus()
+	}
+
+	if *showbackend {
+		haproxy.ShowRegexp("BACKEND")
+	}
+
+	if *showhealth {
+		haproxy.Showhealth()
+	}
+	if *enable != "" {
+		en := strings.Split(*enable, "/")
+		haproxy.EnableServer(en[0], en[1])
+	}
+	if *disable != "" {
+		dis := strings.Split(*disable, "/")
+		haproxy.DisableServer(dis[0], dis[1])
+	} 
+	// flag.PrintDefaults()
 }
